058368df87572f34d9eef50fa9eacebc72665487
Alla Devyatova <alladevyatova@gmail.com>
2025-03-16 23:02:42 +0300

    first_iteration

---
+++ precode_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMainHandlerWhenOk(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?count=2&city=moscow", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	status := responseRecorder.Code
+	assert.Equal(t, http.StatusOK, status)
+}
+
+func TestMainHandlerWhenMissingCount(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?city=moscow", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	status := responseRecorder.Code
+	assert.Equal(t, http.StatusBadRequest, status)
+
+	expected := `count missing`
+	assert.Equal(t, expected, responseRecorder.Body.String())
+}
+
+func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
+	totalCount := 4
+	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	status := responseRecorder.Code
+	assert.Equal(t, http.StatusOK, status)
+
+	body := responseRecorder.Body.String()
+	list := strings.Split(body, ",")
+
+	assert.Equal(t, totalCount, len(list))
+}
